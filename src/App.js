131fff3b2e703cfba77c2333e1b20ca35077dd90
Wes Pearce <wespearce1@gmail.com>
2020-08-05 20:13:32 -0600

    React app started

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1 className="bg-primary">Hi nerd!</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
