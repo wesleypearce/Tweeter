2f01e70d9b7eeb68a296872641db05403d3da000
Wes Pearce <wespearce1@gmail.com>
2020-08-23 20:02:13 -0600

    Added disclaimer on home page

--- src/components/HelloBox.js
+++ src/components/HelloBox.js
@@ -7,13 +7,16 @@ const HelloBox = () => {
       <h1 className="display-1 text-center">Hello!</h1>
 
       <div className="col-lg-6 col-md-5 mx-auto mt-3 mb-3">
-        {/* <button className="btn-block btn-light mx-auto">Sign Up</button>
-        <button className="btn-block btn-light mx-auto">Log In</button> */}
         <Auth />
       </div>
 
       <p className="text-center lead">
         Come see what everyone is talking about!
+      </p>
+      <p className="text-center mt-4">
+        EzTweeter is a fake Twitter. When you log in, you will see your profile
+        but all tweets you make in the EzTweeter app are only displayed here.
+        They are not being updated on Twitter.com.
       </p>
     </div>
   );
